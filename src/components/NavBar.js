922fdc9e82dc2d90015276f7b6235c9a268dfc52
Deivid Almeida <deividalmeida365@gmail.com>
2021-08-17 21:13:54 -0300

    navMenu finished

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
 
 import {NavLink} from 'react-router-dom';
+
 import { MdClose, MdMenu } from 'react-icons/md';
 
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-import 'global.css';
+import '../src/global.css';
 
 
 
